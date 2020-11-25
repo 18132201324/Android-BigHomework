b76e54ebbce01ef6883eb4a47e2363b81f7e81e8
杨志诚 <44825526+chengzhi666@users.noreply.github.com>
2020-11-25 22:47:49 +0800

    Update MyApplication.java

--- app/src/main/java/com/example/smartcommunityapplication/MyApplication.java
+++ app/src/main/java/com/example/smartcommunityapplication/MyApplication.java
@@ -17,6 +17,7 @@ import com.scwang.smartrefresh.layout.header.ClassicsHeader;
 public class MyApplication extends Application {
     private int i;
     private String name;
+    private String n1ame;
 
     @Override
     public void onCreate() {
