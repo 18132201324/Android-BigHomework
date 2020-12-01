fb003115a8af59233167effeec4a84dbc80a7220
SongLingrui <19080762@qq.com>
2020-12-01 21:09:19 +0800

    slr update

--- app/src/main/java/com/example/smartcommunityapplication/activities/MainActivity.java
+++ app/src/main/java/com/example/smartcommunityapplication/activities/MainActivity.java
@@ -4,9 +4,11 @@ import androidx.appcompat.app.AppCompatActivity;
 import androidx.fragment.app.FragmentTabHost;
 import androidx.fragment.app.FragmentTransaction;
 
+import android.graphics.drawable.Drawable;
 import android.os.Bundle;
 import android.util.Log;
 import android.view.View;
+import android.widget.ImageView;
 import android.widget.TabHost;
 import android.widget.TextView;
 
@@ -24,6 +26,7 @@ import java.util.Map;
 
 public class MainActivity extends AppCompatActivity {
     private Map<String,TextView> textViewMap = new HashMap<>();
+    private Map<String, ImageView> imgViewMap = new HashMap<>();
     private FragmentTabHost fragmentTabHost;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -40,7 +43,7 @@ public class MainActivity extends AppCompatActivity {
 
         //创建内容TabSpec对象
         TabHost.TabSpec tab1 = fragmentTabHost.newTabSpec("first_tab")
-                .setIndicator(getTabSpecView("first_tab","首页"));
+                .setIndicator(getTabSpecView("first_tab","首页",getResources().getDrawable(R.drawable.home1)));
 
         //Class参数：类名.class,对象.getClass()
         fragmentTabHost.addTab(tab1,
@@ -48,28 +51,28 @@ public class MainActivity extends AppCompatActivity {
                 null);//传递数据时使用，不需要传递数据直接传null
 
         TabHost.TabSpec tab2 = fragmentTabHost.newTabSpec("second_tab")
-                .setIndicator(getTabSpecView("second_tab","商城"));
+                .setIndicator(getTabSpecView("second_tab","商城",getResources().getDrawable(R.drawable.shop1)));
 
         fragmentTabHost.addTab(tab2,
                 ShopPageFragment.class,
                 null);
 
         TabHost.TabSpec tab3 = fragmentTabHost.newTabSpec("third_tab")
-                .setIndicator(getTabSpecView("third_tab","资讯"));
+                .setIndicator(getTabSpecView("third_tab","资讯",getResources().getDrawable(R.drawable.information1)));
 
         fragmentTabHost.addTab(tab3,
                 InformationPageFragment.class,
                 null);
 
         TabHost.TabSpec tab4 = fragmentTabHost.newTabSpec("forth_tab")
-                .setIndicator(getTabSpecView("forth_tab","通知"));
+                .setIndicator(getTabSpecView("forth_tab","通知",getResources().getDrawable(R.drawable.chat1)));
 
         fragmentTabHost.addTab(tab4,
                 NoticePageFragment.class,
                 null);
 
         TabHost.TabSpec tab5 = fragmentTabHost.newTabSpec("fifth_tab")
-                .setIndicator(getTabSpecView("fifth_tab","我的"));
+                .setIndicator(getTabSpecView("fifth_tab","我的",getResources().getDrawable(R.drawable.mine1)));
 
         fragmentTabHost.addTab(tab5,
                 MyselfPageFragment.class,
@@ -83,39 +86,64 @@ public class MainActivity extends AppCompatActivity {
                 //修改图片和文字颜色
                 switch(tabId){
                     case "first_tab":
-                        textViewMap.get("first_tab").setTextColor(getResources().getColor(R.color.colorPrimary));
+                        textViewMap.get("first_tab").setTextColor(getResources().getColor(R.color.colo_blue_green));
+                        imgViewMap.get("first_tab").setImageDrawable(getResources().getDrawable(R.drawable.home));
                         textViewMap.get("second_tab").setTextColor(getResources().getColor(android.R.color.black));
+                        imgViewMap.get("second_tab").setImageDrawable(getResources().getDrawable(R.drawable.shop1));
                         textViewMap.get("third_tab").setTextColor(getResources().getColor(android.R.color.black));
+                        imgViewMap.get("third_tab").setImageDrawable(getResources().getDrawable(R.drawable.information1));
                         textViewMap.get("forth_tab").setTextColor(getResources().getColor(android.R.color.black));
+                        imgViewMap.get("forth_tab").setImageDrawable(getResources().getDrawable(R.drawable.chat1));
                         textViewMap.get("fifth_tab").setTextColor(getResources().getColor(android.R.color.black));
+                        imgViewMap.get("fifth_tab").setImageDrawable(getResources().getDrawable(R.drawable.mine1));
                         break;
                     case "second_tab":
                         textViewMap.get("first_tab").setTextColor(getResources().getColor(android.R.color.black));
-                        textViewMap.get("second_tab").setTextColor(getResources().getColor(R.color.colorPrimary));
+                        imgViewMap.get("first_tab").setImageDrawable(getResources().getDrawable(R.drawable.home1));
+                        textViewMap.get("second_tab").setTextColor(getResources().getColor(R.color.colo_blue_green));
+                        imgViewMap.get("second_tab").setImageDrawable(getResources().getDrawable(R.drawable.shop));
                         textViewMap.get("third_tab").setTextColor(getResources().getColor(android.R.color.black));
+                        imgViewMap.get("third_tab").setImageDrawable(getResources().getDrawable(R.drawable.information1));
                         textViewMap.get("forth_tab").setTextColor(getResources().getColor(android.R.color.black));
+                        imgViewMap.get("forth_tab").setImageDrawable(getResources().getDrawable(R.drawable.chat1));
                         textViewMap.get("fifth_tab").setTextColor(getResources().getColor(android.R.color.black));
+                        imgViewMap.get("fifth_tab").setImageDrawable(getResources().getDrawable(R.drawable.mine1));
                         break;
                     case "third_tab":
                         textViewMap.get("first_tab").setTextColor(getResources().getColor(android.R.color.black));
+                        imgViewMap.get("first_tab").setImageDrawable(getResources().getDrawable(R.drawable.home1));
                         textViewMap.get("second_tab").setTextColor(getResources().getColor(android.R.color.black));
-                        textViewMap.get("third_tab").setTextColor(getResources().getColor(R.color.colorPrimary));
+                        imgViewMap.get("second_tab").setImageDrawable(getResources().getDrawable(R.drawable.shop1));
+                        textViewMap.get("third_tab").setTextColor(getResources().getColor(R.color.colo_blue_green));
+                        imgViewMap.get("third_tab").setImageDrawable(getResources().getDrawable(R.drawable.information));
                         textViewMap.get("forth_tab").setTextColor(getResources().getColor(android.R.color.black));
+                        imgViewMap.get("forth_tab").setImageDrawable(getResources().getDrawable(R.drawable.chat1));
                         textViewMap.get("fifth_tab").setTextColor(getResources().getColor(android.R.color.black));
+                        imgViewMap.get("fifth_tab").setImageDrawable(getResources().getDrawable(R.drawable.mine1));
                         break;
                     case "forth_tab":
                         textViewMap.get("first_tab").setTextColor(getResources().getColor(android.R.color.black));
+                        imgViewMap.get("first_tab").setImageDrawable(getResources().getDrawable(R.drawable.home1));
                         textViewMap.get("second_tab").setTextColor(getResources().getColor(android.R.color.black));
+                        imgViewMap.get("second_tab").setImageDrawable(getResources().getDrawable(R.drawable.shop1));
                         textViewMap.get("third_tab").setTextColor(getResources().getColor(android.R.color.black));
-                        textViewMap.get("forth_tab").setTextColor(getResources().getColor(R.color.colorPrimary));
+                        imgViewMap.get("third_tab").setImageDrawable(getResources().getDrawable(R.drawable.information1));
+                        textViewMap.get("forth_tab").setTextColor(getResources().getColor(R.color.colo_blue_green));
+                        imgViewMap.get("forth_tab").setImageDrawable(getResources().getDrawable(R.drawable.chat));
                         textViewMap.get("fifth_tab").setTextColor(getResources().getColor(android.R.color.black));
+                        imgViewMap.get("fifth_tab").setImageDrawable(getResources().getDrawable(R.drawable.mine1));
                         break;
                     case "fifth_tab":
                         textViewMap.get("first_tab").setTextColor(getResources().getColor(android.R.color.black));
+                        imgViewMap.get("first_tab").setImageDrawable(getResources().getDrawable(R.drawable.home1));
                         textViewMap.get("second_tab").setTextColor(getResources().getColor(android.R.color.black));
+                        imgViewMap.get("second_tab").setImageDrawable(getResources().getDrawable(R.drawable.shop1));
                         textViewMap.get("third_tab").setTextColor(getResources().getColor(android.R.color.black));
+                        imgViewMap.get("third_tab").setImageDrawable(getResources().getDrawable(R.drawable.information1));
                         textViewMap.get("forth_tab").setTextColor(getResources().getColor(android.R.color.black));
-                        textViewMap.get("fifth_tab").setTextColor(getResources().getColor(R.color.colorPrimary));
+                        imgViewMap.get("forth_tab").setImageDrawable(getResources().getDrawable(R.drawable.chat1));
+                        textViewMap.get("fifth_tab").setTextColor(getResources().getColor(R.color.colo_blue_green));
+                        imgViewMap.get("fifth_tab").setImageDrawable(getResources().getDrawable(R.drawable.mine));
                         break;
                 }
             }
@@ -124,20 +152,25 @@ public class MainActivity extends AppCompatActivity {
         //设置默认选中的标签页：参数是下标
         fragmentTabHost.setCurrentTab(0);
         //setTextColor不能直接传入color资源的id号
-        textViewMap.get("first_tab").setTextColor(getResources().getColor(R.color.colorPrimary));
+        textViewMap.get("first_tab").setTextColor(getResources().getColor(R.color.colo_blue_green));
+        imgViewMap.get("first_tab").setImageDrawable(getResources().getDrawable(R.drawable.home));
     }
 
 
 
-    public View getTabSpecView(String tag, String title){
+    public View getTabSpecView(String tag, String title, Drawable drawable){
         View view = getLayoutInflater().inflate(R.layout.tab_spec_layout,null);
 
         TextView tv_title = view.findViewById(R.id.title);
+        ImageView iv_photo = view.findViewById(R.id.photo);
         //将ImageView对象存储到Map中
         textViewMap.put(tag,tv_title);
+        imgViewMap.put(tag,iv_photo);
 
         TextView tvTitle = view.findViewById(R.id.title);
+        ImageView ivPhoto = view.findViewById(R.id.photo);
         tvTitle.setText(title);
+        ivPhoto.setImageDrawable(drawable);
         return view;
     }
 }
--- app/src/main/java/com/example/smartcommunityapplication/fragments/NoticePageFragment.java
+++ app/src/main/java/com/example/smartcommunityapplication/fragments/NoticePageFragment.java
@@ -1,9 +1,11 @@
 package com.example.smartcommunityapplication.fragments;
 
+import android.graphics.drawable.Drawable;
 import android.os.Bundle;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.ImageView;
 import android.widget.TabHost;
 import android.widget.TextView;
 
@@ -19,6 +21,7 @@ import java.util.Map;
 
 public class NoticePageFragment extends Fragment {
     private Map<String, TextView> textViewMap = new HashMap<>();
+    private Map<String, ImageView> imgViewMap = new HashMap<>();
 
     @Nullable
     @Override
@@ -37,7 +40,7 @@ public class NoticePageFragment extends Fragment {
 
         //创建内容TabSpec对象
         TabHost.TabSpec tab1 = fragmentTabHost.newTabSpec("first_tab")
-                .setIndicator(getTabSpecView("first_tab","聊天"));
+                .setIndicator(getTabSpecView("first_tab","聊天",getResources().getDrawable(R.drawable.weixin1)));
 
         //Class参数：类名.class,对象.getClass()
         fragmentTabHost.addTab(tab1,
@@ -45,14 +48,14 @@ public class NoticePageFragment extends Fragment {
                 null);//传递数据时使用，不需要传递数据直接传null
 
         TabHost.TabSpec tab2 = fragmentTabHost.newTabSpec("second_tab")
-                .setIndicator(getTabSpecView("second_tab","通讯录"));
+                .setIndicator(getTabSpecView("second_tab","通讯录",getResources().getDrawable(R.drawable.information1)));
 
         fragmentTabHost.addTab(tab2,
                 AddressListPageFragment.class,
                 null);
 
         TabHost.TabSpec tab3 = fragmentTabHost.newTabSpec("third_tab")
-                .setIndicator(getTabSpecView("third_tab","朋友圈"));
+                .setIndicator(getTabSpecView("third_tab","朋友圈",getResources().getDrawable(R.drawable.moment1)));
 
         fragmentTabHost.addTab(tab3,
                 MomentPageFragment.class,
@@ -66,18 +69,27 @@ public class NoticePageFragment extends Fragment {
                 switch(tabId){
                     case "first_tab":
                         textViewMap.get("first_tab").setTextColor(getResources().getColor(R.color.colorPrimary));
+                        imgViewMap.get("first_tab").setImageDrawable(getResources().getDrawable(R.drawable.weixin));
                         textViewMap.get("second_tab").setTextColor(getResources().getColor(android.R.color.black));
+                        imgViewMap.get("second_tab").setImageDrawable(getResources().getDrawable(R.drawable.information1));
                         textViewMap.get("third_tab").setTextColor(getResources().getColor(android.R.color.black));
+                        imgViewMap.get("third_tab").setImageDrawable(getResources().getDrawable(R.drawable.moment1));
                         break;
                     case "second_tab":
                         textViewMap.get("first_tab").setTextColor(getResources().getColor(android.R.color.black));
+                        imgViewMap.get("first_tab").setImageDrawable(getResources().getDrawable(R.drawable.weixin1));
                         textViewMap.get("second_tab").setTextColor(getResources().getColor(R.color.colorPrimary));
+                        imgViewMap.get("second_tab").setImageDrawable(getResources().getDrawable(R.drawable.information));
                         textViewMap.get("third_tab").setTextColor(getResources().getColor(android.R.color.black));
+                        imgViewMap.get("third_tab").setImageDrawable(getResources().getDrawable(R.drawable.moment1));
                         break;
                     case "third_tab":
                         textViewMap.get("first_tab").setTextColor(getResources().getColor(android.R.color.black));
+                        imgViewMap.get("first_tab").setImageDrawable(getResources().getDrawable(R.drawable.weixin1));
                         textViewMap.get("second_tab").setTextColor(getResources().getColor(android.R.color.black));
+                        imgViewMap.get("second_tab").setImageDrawable(getResources().getDrawable(R.drawable.information1));
                         textViewMap.get("third_tab").setTextColor(getResources().getColor(R.color.colorPrimary));
+                        imgViewMap.get("third_tab").setImageDrawable(getResources().getDrawable(R.drawable.moment));
                         break;
                 }
             }
@@ -87,18 +99,23 @@ public class NoticePageFragment extends Fragment {
         fragmentTabHost.setCurrentTab(0);
         //setTextColor不能直接传入color资源的id号
         textViewMap.get("first_tab").setTextColor(getResources().getColor(R.color.colorPrimary));
+        imgViewMap.get("first_tab").setImageDrawable(getResources().getDrawable(R.drawable.weixin));
         return view;
     }
 
-    public View getTabSpecView(String tag, String title){
+    public View getTabSpecView(String tag, String title,Drawable drawable){
         View view = getLayoutInflater().inflate(R.layout.tab_spec_layout,null);
 
         TextView tv_title = view.findViewById(R.id.title);
+        ImageView iv_photo = view.findViewById(R.id.photo);
         //将ImageView对象存储到Map中
         textViewMap.put(tag,tv_title);
+        imgViewMap.put(tag,iv_photo);
 
         TextView tvTitle = view.findViewById(R.id.title);
+        ImageView ivPhoto = view.findViewById(R.id.photo);
         tvTitle.setText(title);
+        ivPhoto.setImageDrawable(drawable);
         return view;
     }
 }
