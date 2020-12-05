3b34d06d5025eb6cc909e85813e6e4c6d2bc07f9
86198 <a45581478@qq.com>
2020-12-05 20:12:40 +0800

    yzc 202012.3

--- app/src/main/java/com/example/smartcommunityapplication/activities/MainActivity.java
+++ app/src/main/java/com/example/smartcommunityapplication/activities/MainActivity.java
@@ -12,6 +12,10 @@ import android.widget.ImageView;
 import android.widget.TabHost;
 import android.widget.TextView;
 
+import com.baidu.location.BDAbstractLocationListener;
+import com.baidu.location.BDLocation;
+import com.baidu.location.LocationClient;
+import com.baidu.location.LocationClientOption;
 import com.example.smartcommunityapplication.R;
 import com.example.smartcommunityapplication.fragments.HomePageFragment;
 import com.example.smartcommunityapplication.fragments.InformationPageFragment;
@@ -25,13 +29,90 @@ import java.util.Map;
 
 
 public class MainActivity extends AppCompatActivity {
-    private Map<String,TextView> textViewMap = new HashMap<>();
+    private Map<String, TextView> textViewMap = new HashMap<>();
     private Map<String, ImageView> imgViewMap = new HashMap<>();
     private FragmentTabHost fragmentTabHost;
+    public LocationClient mLocationClient = null;
+    private MyLocationListener myListener = new MyLocationListener();
+
+
+    public class MyLocationListener extends BDAbstractLocationListener {
+        @Override
+        public void onReceiveLocation(BDLocation location) {
+            //此处的BDLocation为定位结果信息类，通过它的各种get方法可获取定位相关的全部结果
+            //以下只列举部分获取经纬度相关（常用）的结果信息
+            //更多结果信息获取说明，请参照类参考中BDLocation类中的说明
+
+            double latitude = location.getLatitude();    //获取纬度信息
+            double longitude = location.getLongitude();    //获取经度信息
+            float radius = location.getRadius();    //获取定位精度，默认值为0.0f
+            Log.e("这个经度是", "" + longitude);
+            Log.e("这个维度是", "" + latitude);
+            String coorType = location.getCoorType();
+            //获取经纬度坐标类型，以LocationClientOption中设置过的坐标类型为准
+
+            int errorCode = location.getLocType();
+            //获取定位类型、定位错误返回码，具体信息可参照类参考中BDLocation类中的说明
+        }
+    }
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        mLocationClient = new LocationClient(getApplicationContext());
+        //声明LocationClient类
+        mLocationClient.registerLocationListener(myListener);
+
+        LocationClientOption option = new LocationClientOption();
+
+        option.setLocationMode(LocationClientOption.LocationMode.Hight_Accuracy);
+//可选，设置定位模式，默认高精度
+//LocationMode.Hight_Accuracy：高精度；
+//LocationMode. Battery_Saving：低功耗；
+//LocationMode. Device_Sensors：仅使用设备；
+
+        option.setCoorType("bd09ll");
+//可选，设置返回经纬度坐标类型，默认GCJ02
+//GCJ02：国测局坐标；
+//BD09ll：百度经纬度坐标；
+//BD09：百度墨卡托坐标；
+//海外地区定位，无需设置坐标类型，统一返回WGS84类型坐标
+
+        option.setScanSpan(1000);
+//可选，设置发起定位请求的间隔，int类型，单位ms
+//如果设置为0，则代表单次定位，即仅定位一次，默认为0
+//如果设置非0，需设置1000ms以上才有效
+
+        option.setOpenGps(true);
+//可选，设置是否使用gps，默认false
+//使用高精度和仅用设备两种定位模式的，参数必须设置为true
+
+        option.setLocationNotify(true);
+//可选，设置是否当GPS有效时按照1S/1次频率输出GPS结果，默认false
+
+        option.setIgnoreKillProcess(false);
+//可选，定位SDK内部是一个service，并放到了独立进程。
+//设置是否在stop的时候杀死这个进程，默认（建议）不杀死，即setIgnoreKillProcess(true)
+
+        option.SetIgnoreCacheException(false);
+//可选，设置是否收集Crash信息，默认收集，即参数为false
+
+        option.setWifiCacheTimeOut(5 * 60 * 1000);
+//可选，V7.2版本新增能力
+//如果设置了该接口，首次启动定位时，会先判断当前Wi-Fi是否超出有效期，若超出有效期，会先重新扫描Wi-Fi，然后定位
+
+        option.setEnableSimulateGps(false);
+//可选，设置是否需要过滤GPS仿真结果，默认需要，即参数为false
+
+        option.setNeedNewVersionRgc(true);
+//可选，设置是否需要最新版本的地址信息。默认需要，即参数为true
+
+        mLocationClient.setLocOption(option);
+//mLocationClient为第二步初始化过的LocationClient对象
+//需将配置好的LocationClientOption对象，通过setLocOption方法传递给LocationClient对象使用
+//更多LocationClientOption的配置，请参照类参考中LocationClientOption类的详细说明
+        mLocationClient.start();
 
 
         //获取FragmentTabHost的引用
@@ -43,7 +124,7 @@ public class MainActivity extends AppCompatActivity {
 
         //创建内容TabSpec对象
         TabHost.TabSpec tab1 = fragmentTabHost.newTabSpec("first_tab")
-                .setIndicator(getTabSpecView("first_tab","首页",getResources().getDrawable(R.drawable.home1)));
+                .setIndicator(getTabSpecView("first_tab", "首页", getResources().getDrawable(R.drawable.home1)));
 
         //Class参数：类名.class,对象.getClass()
         fragmentTabHost.addTab(tab1,
@@ -51,28 +132,28 @@ public class MainActivity extends AppCompatActivity {
                 null);//传递数据时使用，不需要传递数据直接传null
 
         TabHost.TabSpec tab2 = fragmentTabHost.newTabSpec("second_tab")
-                .setIndicator(getTabSpecView("second_tab","商城",getResources().getDrawable(R.drawable.shop1)));
+                .setIndicator(getTabSpecView("second_tab", "商城", getResources().getDrawable(R.drawable.shop1)));
 
         fragmentTabHost.addTab(tab2,
                 ShopPageFragment.class,
                 null);
 
         TabHost.TabSpec tab3 = fragmentTabHost.newTabSpec("third_tab")
-                .setIndicator(getTabSpecView("third_tab","资讯",getResources().getDrawable(R.drawable.information1)));
+                .setIndicator(getTabSpecView("third_tab", "资讯", getResources().getDrawable(R.drawable.information1)));
 
         fragmentTabHost.addTab(tab3,
                 InformationPageFragment.class,
                 null);
 
         TabHost.TabSpec tab4 = fragmentTabHost.newTabSpec("forth_tab")
-                .setIndicator(getTabSpecView("forth_tab","通知",getResources().getDrawable(R.drawable.chat1)));
+                .setIndicator(getTabSpecView("forth_tab", "通知", getResources().getDrawable(R.drawable.chat1)));
 
         fragmentTabHost.addTab(tab4,
                 NoticePageFragment.class,
                 null);
 
         TabHost.TabSpec tab5 = fragmentTabHost.newTabSpec("fifth_tab")
-                .setIndicator(getTabSpecView("fifth_tab","我的",getResources().getDrawable(R.drawable.mine1)));
+                .setIndicator(getTabSpecView("fifth_tab", "我的", getResources().getDrawable(R.drawable.mine1)));
 
         fragmentTabHost.addTab(tab5,
                 MyselfPageFragment.class,
@@ -84,7 +165,7 @@ public class MainActivity extends AppCompatActivity {
             @Override
             public void onTabChanged(String tabId) {
                 //修改图片和文字颜色
-                switch(tabId){
+                switch (tabId) {
                     case "first_tab":
                         textViewMap.get("first_tab").setTextColor(getResources().getColor(R.color.colo_blue_green));
                         imgViewMap.get("first_tab").setImageDrawable(getResources().getDrawable(R.drawable.home));
@@ -157,15 +238,14 @@ public class MainActivity extends AppCompatActivity {
     }
 
 
-
-    public View getTabSpecView(String tag, String title, Drawable drawable){
-        View view = getLayoutInflater().inflate(R.layout.tab_spec_layout,null);
+    public View getTabSpecView(String tag, String title, Drawable drawable) {
+        View view = getLayoutInflater().inflate(R.layout.tab_spec_layout, null);
 
         TextView tv_title = view.findViewById(R.id.title);
         ImageView iv_photo = view.findViewById(R.id.photo);
         //将ImageView对象存储到Map中
-        textViewMap.put(tag,tv_title);
-        imgViewMap.put(tag,iv_photo);
+        textViewMap.put(tag, tv_title);
+        imgViewMap.put(tag, iv_photo);
 
         TextView tvTitle = view.findViewById(R.id.title);
         ImageView ivPhoto = view.findViewById(R.id.photo);
